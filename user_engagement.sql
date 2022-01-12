ec4f3b3c682de3cf1e21e56a7b0b3acdae039a08
JC <57039610+jacyee@users.noreply.github.com>
2022-01-13 01:59:25 +0800

    Update user_engagement.sql

--- user_engagement.sql
+++ user_engagement.sql
@@ -6,4 +6,10 @@ WHERE e.event_type = 'engagement'
 GROUP BY 1
 ORDER BY 1 
 
-
+-- Activation rate as engagement level
+SELECT DATE_TRUNC('day',created_at) AS day_of_week,
+COUNT(CASE WHEN activated_at IS NOT NULL THEN u.user_id ELSE NULL END) AS activated_users
+  FROM tutorial.yammer_users u
+  where created_at > '2014-05-01' and created_at<'2014-08-04'
+ GROUP BY 1
+ ORDER BY 1
