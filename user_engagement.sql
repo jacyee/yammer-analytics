69f4af0f122917567253b32759f0de8b3812884a
JC <57039610+jacyee@users.noreply.github.com>
2022-01-13 01:15:31 +0800

    Create user_engagement.sql

---
+++ user_engagement.sql
@@ -0,0 +1,7 @@
+-- weekly active user as engagement level
+SELECT DATE_TRUNC('week', e.occurred_at) as day_of_week,
+       COUNT(DISTINCT e.user_id) AS weekly_active_users
+FROM tutorial.yammer_events e
+WHERE e.event_type = 'engagement'
+GROUP BY 1
+ORDER BY 1 
