2ebfe8cdaaf9958b1a5f809a957c6a1648c89740
JC <57039610+jacyee@users.noreply.github.com>
2022-01-13 15:33:53 +0800

    Update user_engagement.sql

--- user_engagement.sql
+++ user_engagement.sql
@@ -1,12 +1,12 @@
--- weekly active user as engagement level
-SELECT DATE_TRUNC('week', e.occurred_at) as day_of_week,
-       COUNT(DISTINCT e.user_id) AS weekly_active_users
+-- Daily active user as engagement level
+SELECT DATE_TRUNC('day', e.occurred_at) as day_of_week,
+       COUNT(DISTINCT e.user_id) AS daily_active_users
 FROM tutorial.yammer_events e
 WHERE e.event_type = 'engagement'
 GROUP BY 1
 ORDER BY 1 
 
--- Activation rate as engagement level
+-- Activated user as engagement level
 SELECT date_trunc('day',created_at) as day_of_week,
 COUNT(case when activated_at is not null THEN u.user_id else null end) as activated_users
   FROM tutorial.yammer_users u
