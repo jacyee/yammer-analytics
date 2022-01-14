5f9a09d07f8362b7bdae10bd8ce79728a10b15d2
JC <57039610+jacyee@users.noreply.github.com>
2022-01-15 01:40:39 +0800

    Update user_engagement.sql

--- user_engagement.sql
+++ user_engagement.sql
@@ -41,3 +41,23 @@ SELECT DATE_TRUNC('week',t.occurred_at) AS "week",
  GROUP BY 1
  ORDER BY 1
  limit 50
+ 
+ 
+ -- engagement of user by device type
+SELECT DATE_TRUNC('week', occurred_at) AS week,
+       COUNT(DISTINCT e.user_id) AS weekly_active_users,
+       COUNT(DISTINCT CASE WHEN e.device IN ('macbook pro','lenovo thinkpad','macbook air','dell inspiron notebook',
+          'asus chromebook','dell inspiron desktop','acer aspire notebook','hp pavilion desktop','acer aspire desktop','mac mini')
+          THEN e.user_id ELSE NULL END) AS desktop,
+       COUNT(DISTINCT CASE WHEN e.device IN ('iphone 5','samsung galaxy s4','nexus 5','iphone 5s','iphone 4s','nokia lumia 635',
+       'htc one','samsung galaxy note','amazon fire phone') THEN e.user_id ELSE NULL END) AS mobile,
+        COUNT(DISTINCT CASE WHEN e.device IN ('ipad air','nexus 7','ipad mini','nexus 10','kindle fire','windows surface',
+        'samsumg galaxy tablet') THEN e.user_id ELSE NULL END) AS tablet
+  FROM tutorial.yammer_events e
+ WHERE e.event_type = 'engagement'
+   AND e.event_name = 'login'
+ GROUP BY 1
+ ORDER BY 1
+LIMIT 100
+
+
