ddeeaf1cd52d1225ee96db3c3fbb7e679d57aeed
JC <57039610+jacyee@users.noreply.github.com>
2022-01-15 13:15:29 +0800

    Update user_engagement.sql

--- user_engagement.sql
+++ user_engagement.sql
@@ -60,4 +60,12 @@ SELECT DATE_TRUNC('week', occurred_at) AS week,
  ORDER BY 1
 LIMIT 100
 
-
+ -- engagement of user by email action type
+SELECT DATE_TRUNC('week', occurred_at) AS week,
+       COUNT(CASE WHEN e.action = 'sent_weekly_digest' THEN e.user_id ELSE NULL END) AS weekly_email,
+       COUNT(CASE WHEN e.action = 'sent_reengagement_email' THEN e.user_id ELSE NULL END) AS reengagement_email,
+       COUNT(CASE WHEN e.action = 'email_open' THEN e.user_id ELSE NULL END) AS email_open,
+       COUNT(CASE WHEN e.action = 'email_clickthrough' THEN e.user_id ELSE NULL END) AS email_clickthrough
+  FROM tutorial.yammer_emails e
+ GROUP BY 1
+ ORDER BY 1
